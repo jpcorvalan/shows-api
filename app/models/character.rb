29abf45288d88938668d951e11e75dc2f201b48b
Juan Pablo Corvalán <juanpablocorvalan98@gmail.com>
2024-03-02 07:14:00 -0300

    added models validation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ApplicationRecord
   has_and_belongs_to_many :shows
+
+  validates :age, presence: true
+  validates :history, presence: true
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :weight, presence: true
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ApplicationRecord
   has_and_belongs_to_many :characters
+
+  validates :image, presence: true
+  validates :rating, presence: true
+  validates :title, presence: true
 end
